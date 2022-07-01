65dffe202d798502063af5d8a1709bcbc12c7f7e
Rishabh Tripathi <86038495+Rishabh-Tripathi1@users.noreply.github.com>
2022-07-01 11:24:45 +0530

    Update Food.py

--- Food.py
+++ Food.py
@@ -61,5 +61,17 @@ print('Random Forest:', accuracy_score(y_test, rfcy_predict))
 print('Decision Tree:', accuracy_score(y_test, dtcy_predict))
 print('Support Vector:', accuracy_score(y_test, svmy_predict))
 print('MLP:', accuracy_score(y_test,  mlpy_predict))
+
+
+'''
+Accuracy Score:
+Logistic: 0.7870521684475172
+Random Forest: 0.7516027655562539
+Decision Tree: 0.619610307982401
+Support Vector: 0.8049025769956003
+MLP: 0.6070395977372721
+Gradient Boosting: 0.6357008170961659
+Naive Bayes: 0.6788183532369579
+'''
 print('Gradient Boosting:', accuracy_score(y_test,  gbcy_predict))
 print('Naive Bayes:', accuracy_score(y_test,  nby_predict))
